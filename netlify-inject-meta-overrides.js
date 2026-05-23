7ff6a26ff5323d7a01f2264708f11b2aa66a38f1
ManTelHD <matthiasmantler24@gmail.com>
2026-05-23 21:47:19 +0200

    Bump compact weapon image cache key

--- netlify-inject-meta-overrides.js
+++ netlify-inject-meta-overrides.js
@@ -7,7 +7,7 @@ const styleMarker = '<link rel="stylesheet" href="meta-overrides.css">';
 const scriptMarker = '<script src="meta-overrides.js"></script>';
 const animationScriptMarker = '<script src="meta-card-animations.js?v=20260523-card-clean1"></script>';
 const pickrateScriptMarker = '<script src="pickrate-bars.js?v=20260523-pickrate1"></script>';
-const weaponZoomScriptMarker = '<script src="weapon-image-zoom.js?v=20260523-weaponmatch-a-tier1"></script>';
+const weaponZoomScriptMarker = '<script src="weapon-image-zoom.js?v=20260523-weapon-a-tier-size1"></script>';
 const mw4ScriptMarker = '<script src="mw4-update.js?v=20260523-mw4-keyart"></script>';
 
 let html = fs.readFileSync(indexPath, "utf8");
