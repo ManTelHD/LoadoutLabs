8d2ee530a9d098651d5b144f5d96e32af4483d49
ManTelHD <matthiasmantler24@gmail.com>
2026-05-23 19:39:09 +0200

    Inject MW4 update script during deploy

--- netlify-inject-meta-overrides.js
+++ netlify-inject-meta-overrides.js
@@ -5,6 +5,7 @@ const siteDir = path.join(__dirname, "New project 2");
 const indexPath = path.join(siteDir, "index.html");
 const styleMarker = '<link rel="stylesheet" href="meta-overrides.css">';
 const scriptMarker = '<script src="meta-overrides.js"></script>';
+const mw4ScriptMarker = '<script src="mw4-update.js?v=20260523-mw4"></script>';
 
 let html = fs.readFileSync(indexPath, "utf8");
 if (!html.includes(styleMarker)) {
@@ -15,5 +16,8 @@ if (!html.includes(scriptMarker)) {
     '    <script src="script.js"></script>',
     `    <script src="script.js"></script>\n    ${scriptMarker}`,
   );
+}
+if (!html.includes(mw4ScriptMarker)) {
+  html = html.replace("</body>", `    ${mw4ScriptMarker}\n  </body>`);
 }
 fs.writeFileSync(indexPath, html);
