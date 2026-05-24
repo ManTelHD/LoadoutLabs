3281a97776398e189f6cb292e3acb709e25a6ace
ManTelHD <matthiasmantler24@gmail.com>
2026-05-24 19:35:46 +0200

    Load interactive loadout comparison

--- netlify-inject-meta-overrides.js
+++ netlify-inject-meta-overrides.js
@@ -24,6 +24,7 @@ const activeScripts = [
   "attachment-levels.js?v=20260524-attachment-levels1",
   "umlaut-polish.js?v=20260524-umlauts6",
   "loadout-details-polish.js?v=20260524-details-polish1",
+  "loadout-compare.js?v=20260524-compare1",
 ];
 
 const retiredScripts = [
