feb9e2f26b70b05844da07a4bcc47d089a1629ac
ManTelHD <matthiasmantler24@gmail.com>
2026-06-06 09:30:06 +0200

    Load final weapon image fixes

--- netlify-inject-meta-overrides.js
+++ netlify-inject-meta-overrides.js
@@ -23,6 +23,7 @@ const activeScripts = [
   withVersion("performance.js", "20260529-perf-grid1"),
   withVersion("loadout-card-polish.js", "20260606-card-polish3"),
   withVersion("loadout-card-finalizer.js", "20260606-card-finalizer1"),
+  withVersion("loadout-image-fix.js", "20260606-image-fix1"),
 ];
 
 const scriptNames = [
